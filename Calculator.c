9366f980032beefb77efc693185d6a05ae39fff3
VikashkrVerma <95168790+VikashkrVerma@users.noreply.github.com>
2022-07-27 23:31:22 +0530

    Addition

---
+++ Calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Enter Two nos ");
+   int a,b;
+   scanf("%d%d",&a,&b);
+   int c = a+b;
+   printf("Result is %d",c);
+
+    return 0;
+}
